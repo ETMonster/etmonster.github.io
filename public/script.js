f6ea5678cf81b5287810d4f05d7417ab0bc16a69
Emmett Tackaberry <emmett.tackaberry@gmail.com>
2023-04-10 21:20:09 -0400

    Found style and started to setup page slide

---
+++ public/script.js
@@ -0,0 +1,7 @@
+const pages = document.querySelectorAll(pages);
+const currentPageNumber = 0;
+
+slide = (requestedPage) => {
+    currentPageNumber = requestedPage;
+    pages.style.transform
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    './public/index.html',
+    './src/**/*.{html,js}',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
