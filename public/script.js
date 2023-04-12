bcb490550745cf60b8310c7fc453da411abdf2ba
Emmett Tackaberry <emmett.tackaberry@gmail.com>
2023-04-11 21:45:25 -0400

    Added accordions and started the Work page

--- public/script.js
+++ public/script.js
@@ -5,3 +5,20 @@ function slide(requestedPage) {
         page.style.transform = `translateX(${-(requestedPage * 100)}%)`
     });
 }
+
+// accordions
+
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(question => {
+    question.addEventListener('click', () => {
+        question.parentElement.classList.toggle('active');
+        
+        if (question.parentElement.classList.contains('active')) {
+            question.querySelectorAll('.accordion-identifier')[0].innerHTML = '▼'
+        }
+        else {
+            question.querySelectorAll('.accordion-identifier')[0].innerHTML = '▲';
+        }
+    });
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,9 @@ module.exports = {
     './src/**/*.{html,js}',
   ],
   theme: {
-    extend: {},
+    extend: {
+
+    },
   },
   plugins: [],
 }
