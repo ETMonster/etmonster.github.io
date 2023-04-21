16711d082e721f0b52ef1e7467cd4e4d44da46a3
Emmett Tackaberry <emmett.tackaberry@gmail.com>
2023-04-21 17:22:50 -0400

    Added background, animations and rest of portfolio

--- public/script.js
+++ public/script.js
@@ -1,24 +1,53 @@
 const pages = document.querySelectorAll('.page');
+const title = document.querySelectorAll('.title')[0];
+const a = document.querySelectorAll('a');
+const iframe = document.querySelectorAll('iframe');
+let hasPressedKey = false;
+let canPressKey = false;
 
-function slide(requestedPage) {  
-    pages.forEach( page => {
-        page.style.transform = `translateX(${-(requestedPage * 100)}%)`
+document.body.style.overflowY = 'hidden';
+a.forEach(i => {i.tabIndex = -1});
+iframe.forEach(i => {i.tabIndex = -1});
+
+window.onbeforeunload = function() {
+    pages.forEach(page => {
+        page.style.transition = 'none';
+        page.style.transform = 'translateX(100%)';
+        title.style.transform = 'translateY(0)';
+        title.style.opacity = 100;
+        scrollTo(document.body.scrollLeft, 0);
     });
 }
 
-// accordions
+function waitForTitleAnimation() {canPressKey = true;}
+setInterval(waitForTitleAnimation, 2000);
 
-const questions = document.querySelectorAll('.question');
+function changePage() {
+    pages.forEach(page => {page.style.transform = 'translateX(-100%)';});
+    a.forEach(i => {i.tabIndex = 0});
+    iframe.forEach(i => {i.tabIndex = 0});
 
-questions.forEach(question => {
-    question.addEventListener('click', () => {
-        question.parentElement.classList.toggle('active');
-        
-        if (question.parentElement.classList.contains('active')) {
-            question.querySelectorAll('.accordion-identifier')[0].innerHTML = '▼'
-        }
-        else {
-            question.querySelectorAll('.accordion-identifier')[0].innerHTML = '▲';
-        }
+    document.body.style.overflowY = 'scroll';
+}
+
+function onKeyPress(e) {
+    pages.forEach(page => {page.style.transition = 'all 2000ms';});
+    if (!hasPressedKey && canPressKey) {
+        title.style.transform = 'translateY(100vh)';
+        title.style.opacity = 0;
+        setInterval(changePage, 2200);
+        hasPressedKey = true;
+    }
+}
+
+document.addEventListener('keypress', onKeyPress);
+
+// links
+
+const workDivs = document.querySelectorAll('.work');
+
+workDivs.forEach(div => {
+    div.addEventListener('click', function() {
+        window.open(div.querySelector('a').getAttribute('href'), '_blank'); 
     });
 });
